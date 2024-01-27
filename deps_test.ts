ddb2a34421fd704232677c8d4d3338c7409d2d61
Kitson Kelly <me@kitsonkelly.com>
2024-01-27 12:56:08 +1100

    chore: update to std 0.213

--- deps_test.ts
+++ deps_test.ts
@@ -8,9 +8,9 @@
 
 export {
   assertEquals,
-} from "https://deno.land/std@0.211.0/assert/assert_equals.ts";
-export { assertInstanceOf } from "https://deno.land/std@0.211.0/assert/assert_instance_of.ts";
-export { assertRejects } from "https://deno.land/std@0.211.0/assert/assert_rejects.ts";
-export { assertThrows } from "https://deno.land/std@0.211.0/assert/assert_throws.ts";
-export { FakeTime } from "https://deno.land/std@0.211.0/testing/time.ts";
-export { KeyStack } from "https://deno.land/std@0.211.0/crypto/unstable_keystack.ts";
+} from "https://deno.land/std@0.213.0/assert/assert_equals.ts";
+export { assertInstanceOf } from "https://deno.land/std@0.213.0/assert/assert_instance_of.ts";
+export { assertRejects } from "https://deno.land/std@0.213.0/assert/assert_rejects.ts";
+export { assertThrows } from "https://deno.land/std@0.213.0/assert/assert_throws.ts";
+export { FakeTime } from "https://deno.land/std@0.213.0/testing/time.ts";
+export { KeyStack } from "https://deno.land/std@0.213.0/crypto/unstable_keystack.ts";
