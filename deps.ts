b6e934d143aff18cb7e04b6cabc8b0f9d3bf2269
Kitson Kelly <me@kitsonkelly.com>
2024-02-14 13:04:09 +1100

    chore: update to std 0.215.0

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 // Copyright 2018-2022 the oak authors. All rights reserved. MIT license.
 
-export { assert } from "https://deno.land/std@0.213.0/assert/assert.ts";
-export * as base64 from "https://deno.land/std@0.213.0/encoding/base64.ts";
-export { typeByExtension } from "https://deno.land/std@0.213.0/media_types/type_by_extension.ts";
+export { assert } from "https://deno.land/std@0.215.0/assert/assert.ts";
+export * as base64 from "https://deno.land/std@0.215.0/encoding/base64.ts";
+export { typeByExtension } from "https://deno.land/std@0.215.0/media_types/type_by_extension.ts";
