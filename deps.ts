8e17f21924f76b0b63b811a9ebfcbe1b39bb0342
Kitson Kelly <me@kitsonkelly.com>
2024-03-05 10:26:20 +1100

    chore: update to std 0.218

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 // Copyright 2018-2022 the oak authors. All rights reserved. MIT license.
 
-export { assert } from "jsr:@std/assert@0.215/assert";
-export * as base64 from "jsr:@std/encoding@0.215/base64";
-export { typeByExtension } from "jsr:@std/media-types@0.215/type_by_extension";
+export { assert } from "jsr:@std/assert@0.218/assert";
+export * as base64 from "jsr:@std/encoding@0.218/base64";
+export { typeByExtension } from "jsr:@std/media-types@0.218/type_by_extension";
--- deps_test.ts
+++ deps_test.ts
@@ -6,10 +6,10 @@
  * @module
  */
 
-export { assertEquals } from "jsr:@std/assert@0.215/assert_equals";
-export { assertInstanceOf } from "jsr:@std/assert@0.215/assert_instance_of";
-export { assertRejects } from "jsr:@std/assert@0.215/assert_rejects";
-export { assertStrictEquals } from "jsr:@std/assert@0.215/assert_strict_equals";
-export { assertThrows } from "jsr:@std/assert@0.215/assert_throws";
-export { FakeTime } from "jsr:@std/testing@0.215/time";
-export { KeyStack } from "jsr:@std/crypto@0.215/unstable_keystack";
+export { assertEquals } from "jsr:@std/assert@0.218/assert_equals";
+export { assertInstanceOf } from "jsr:@std/assert@0.218/assert_instance_of";
+export { assertRejects } from "jsr:@std/assert@0.218/assert_rejects";
+export { assertStrictEquals } from "jsr:@std/assert@0.218/assert_strict_equals";
+export { assertThrows } from "jsr:@std/assert@0.218/assert_throws";
+export { FakeTime } from "jsr:@std/testing@0.218/time";
+export { KeyStack } from "jsr:@std/crypto@0.218/unstable_keystack";
